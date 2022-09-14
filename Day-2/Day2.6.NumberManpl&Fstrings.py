cde0a7bc4e70b29d85f716ddc12fbc6ed0f501fc
srirath7 <srirathreddy@gmail.com>
2022-09-14 20:59:02 +0530

    Day2-Python

---
+++ Day-2/Day2.6.NumberManpl&Fstrings.py
@@ -0,0 +1,21 @@
+#create a round of decimal at 2 digits
+print(round(8/3,2))
+
+
+# Double div (or) chopping gives quotation
+print(10//3)
+
+#result again dividing
+result = 4/2
+result /=2
+print(result)
+
+#score recording
+#say s = score
+s = 0
+s +=1
+s -=1
+s +=6
+s +=4
+s +=3
+print("score:",s)
