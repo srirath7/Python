c69525cf8e599c65e77c073b3cad6294d5284f92
srirath7 <srirathreddy@gmail.com>
2022-09-15 12:34:22 +0530

    day3 update1

---
+++ Day-3/Day3.2.ifelse&conditional.py
@@ -0,0 +1,20 @@
+# If else statements
+# Create rollercoaster! height checker if height is leass than 120 
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+else:
+    print("Sorry, you have to grow taller to ride.") 
+    
+
+######################
+#Comparision
+#   >   greater than
+#   <   less than
+#   >=  greater than or equal to
+#   <=  less than or equal to
+#   ==  obsolute equal to
+#   !=  not equal to
+#######################
