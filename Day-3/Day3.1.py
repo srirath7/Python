b163b74e0b6bd4b301002e42057318311f461324
srirath7 <srirathreddy@gmail.com>
2022-09-15 12:12:33 +0530

    Day3 started

---
+++ Day-3/Day3.1.py
@@ -0,0 +1,11 @@
+#Conditional statements, Logic operators, Code blocks and scope
+
+#Welcome to Treasure Island
+#Your mission is to find the trasure
+# you areat a cross road. where do you want to go? "left" or "right"
+
+#you come to lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.
+
+#you arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. which colour do you choose?
+
+#It's a room of fire. Game over
